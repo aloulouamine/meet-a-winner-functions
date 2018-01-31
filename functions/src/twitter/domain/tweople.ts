a938c1910338b8e1dafacfe981732266a2c65741
François GRUCHALA <@FGruchala>
2018-01-31 23:55:50 +0100

    :sparkles: : add description to tweople domain

--- functions/src/twitter/domain/tweople.ts
+++ functions/src/twitter/domain/tweople.ts
@@ -2,6 +2,7 @@ export class Tweople {
 
     constructor(public id_str?: string,
                 public name?: string,
+                public description?: string,
                 public profile_image_url_https?: string,
                 public profile_background_image_url_https?: string) {
     }
