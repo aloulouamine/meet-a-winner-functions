794f5fddab5c7dc63807bad79ae90f7bbffc538d
François GRUCHALA <@FGruchala>
2018-01-31 23:56:19 +0100

    :sparkles: : get rsvps from meet up

---
+++ functions/src/meetup/domain/rsvp.ts
@@ -0,0 +1,8 @@
+export class Rsvp {
+
+    constructor(public rsvp_id?: string,
+                public member?: any,
+                public member_photo?: any) {
+    }
+
+}
--- functions/src/meetup/service/meetup.service.ts
+++ functions/src/meetup/service/meetup.service.ts
@@ -2,6 +2,7 @@ import * as functions from 'firebase-functions';
 import * as MeetupApi from 'meetup-api';
 import {Exception} from '../../shared/domain/exception';
 import {Meetup} from '../domain/meetup';
+import {Rsvp} from '../domain/rsvp';
 
 /**
  * Manage interactions with Meetup platform.
@@ -33,6 +34,29 @@ export class MeetupService {
 
                         const meetups = data.results.map(meetup => <Meetup>meetup);
                         resolve(meetups);
+                    });
+
+            });
+    }
+
+    /**
+     * Retrieve a list of {@link Rsvp} via the id of a {@link Meetup}.
+     * @param {string} Id of a {@link Meetup}
+     * @param {string}
+     * @returns {Promise<Array<Rsvp>>}
+     */
+    public getRsvpsOfAMeetup(meetupId: string, rsvpType: string = "yes"): Promise<Array<Rsvp>> {
+        return new Promise(
+            (resolve: (value: Array<Rsvp>) => void, reject: (reason: Exception) => void): void => {
+
+                this.meetupClient.getRSVPs({event_id: meetupId, rsvp: rsvpType},
+                    (err, data) => {
+                        if (err) {
+                            reject(new Exception(`Error retrieving rsvp(s) from meetup ${meetupId} from the Meetup platform.`));
+                        }
+
+                        const rsvps = data.results.map(rsvp => <Rsvp>rsvp);
+                        resolve(rsvps);
                     });
 
             });
